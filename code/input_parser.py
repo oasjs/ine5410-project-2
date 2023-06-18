53e1bc3c8c4d40b42227506efd6ae7408366407a
dexeme <dexe.me12@gmail.com>
2023-06-18 20:22:41 -0300

    Implementa função col_parser

--- code/input_parser.py
+++ code/input_parser.py
@@ -17,8 +17,16 @@ class InputParser:
     def row_parser(self) -> list[str]:
         return self.remove_line_breaks(["534678912\n", "672195348\n", "198342567\n", "859761423\n", "426853791\n", "713924856\n", "961537284\n", "287419635\n", "345286179\n"])
 
-    def col_parser(self):
-        pass
+    def col_parser(self) -> list[str]:
+        tabuleiro_col_major = []
+        column = ""
+        tabuleiro_row_major = self.remove_line_breaks(["534678912\n", "672195348\n", "198342567\n", "859761423\n", "426853791\n", "713924856\n", "961537284\n", "287419635\n", "345286179\n"])
+        for i in range(9):
+            for j in range(0, 9):
+                column += tabuleiro_row_major[j][i]
+            print(column)
+            tabuleiro_col_major.append(column)
+            column = ""
 
     def region_parser(self):
         pass
