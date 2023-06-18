99f5b05564d72e6aaf8fe4c93457c5fb71cfe414
dexeme <dexe.me12@gmail.com>
2023-06-18 20:19:52 -0300

    Implementa função que remove os "\n" da lista

--- code/input_parser.py
+++ code/input_parser.py
@@ -8,8 +8,11 @@ class InputParser:
         self.__board: Board = Board()
 
     def remove_line_breaks(self, board_set: list[str]) -> list[str]:
-        
-        pass
+        for i in range(len(board_set)):
+            board_set[i] = board_set[i].replace("\n", "")
+            print(board_set[i])
+        print('Fim do print do Remove Line Breaks\n')
+        return board_set
 
     def row_parser(self) -> list[str]:
         pass
