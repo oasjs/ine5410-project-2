e13d7f90913640b277aac86967ac1cffe7b06a3c
dexeme <dexe.me12@gmail.com>
2023-06-18 20:21:20 -0300

    Implementa função row_parser
    Essa função apenas chama a função de remover as quebras de linha e retorna essa lista, pois o tabuleiro já vem organizado na forma row_major

--- code/input_parser.py
+++ code/input_parser.py
@@ -15,7 +15,7 @@ class InputParser:
         return board_set
 
     def row_parser(self) -> list[str]:
-        pass
+        return self.remove_line_breaks(["534678912\n", "672195348\n", "198342567\n", "859761423\n", "426853791\n", "713924856\n", "961537284\n", "287419635\n", "345286179\n"])
 
     def col_parser(self):
         pass
