57c4ddf0afe39b9a8f37647b873945e61dbb7b54
oasjs <oasjatoba1@gmail.com>
2023-06-18 17:16:28 -0300

    Implementa lógica da main

--- code/main.py
+++ code/main.py
@@ -1,19 +1,50 @@
 import sys
-import os
-import multiprocessing as mp
-import threading as th
-
-# TODO: Implementar leitura de argumentos
-
-# TODO: Implementar criação dos processos
-
-# TODO: Implementar leitura do arquivo
-
-# TODO: Implementar divisão dos tabuleiros entre processos
+from concurrent.futures import ProcessPoolExecutor
+from process_controller import ProcessController
+from input_parser import InputParser
 
 
-def main():
-    pass
+def main() -> int:
+    n_processes = int(sys.argv[1])
+    n_threads = int(sys.argv[2])
 
-def run():
-    pass
+    if (len(sys.argv)) < 3:
+        print("Tá errado!")
+        return 1
+
+    with open('input-sample.txt', 'r') as f:
+        input = f.readlines()
+
+    # Remove os elementos quebra-de-linha da lista
+    input = [i for i in input if i != '\n']
+
+    n_boards = len(input) // 9
+
+    # Divide input em tabuleiros
+    input = [input[i:i+9] for i in range(0, len(input), 9)]
+
+    # Limita o número máximo de processos ao número de tabuleiros
+    if n_processes > n_boards:
+        n_processes = n_boards
+
+    # Inicaliza as variáveis para a distribuição de pombal
+    step = n_boards // n_processes
+    mod = n_boards % n_processes
+    prev_end = 0
+
+    with ProcessPoolExecutor(max_workers=n_processes) as e:
+        for i in range(n_processes):
+            start = prev_end
+            end = prev_end + step + 1 if mod > 0 else prev_end + step
+            board_sets = input[start:end]
+
+            prev_end = end
+            if mod > 0:
+                mod -= 1
+
+            e.submit(ProcessController(n_threads, board_sets).start())
+
+    return 0
+
+
+main()
