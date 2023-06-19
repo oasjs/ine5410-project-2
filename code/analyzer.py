5397130be5cbf0d428c611f03ab66733dd38f045
MarcosLacava <marcoslaurean@gmail.com>
2023-06-18 21:57:38 -0300

    Cria a função start em Analizer

--- code/analyzer.py
+++ code/analyzer.py
@@ -1,14 +1,20 @@
+from typing import Any
+from board import Board
+
 # TODO: Implementar verificação dos erros para cada tipo de perocrrimento
 
 class Analyzer:
     def __init__(self) -> None:
-        self.__errors: str = ""
-
-    def check_row(self):
         pass
 
-    def check_column(self):
+    def start(self, board : Board):
+        errors = list[str]
+
+    def check_row(self, board : list[str]):
         pass
 
-    def check_region(self):
+    def check_column(self, board : list[str]):
+        pass
+
+    def check_region(self, board : list[str]):
         pass
