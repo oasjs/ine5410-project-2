0a4aecdf794b94601526514b62a2d21a643af77a
MarcosLacava <marcoslaurean@gmail.com>
2023-06-24 13:42:51 -0300

    Implementa função start e check em Analyzer

--- code/analyzer.py
+++ code/analyzer.py
@@ -8,13 +8,33 @@ class Analyzer:
         pass
 
     def start(self, board : Board):
-        errors = list[str]
+        errors = list()
 
-    def check_row(self, board : list[str]):
-        pass
+        # Verifica erros nas linhas
+        for i in range(9):
+            c = self.check(board.row_major[i])
+            for e in c:
+                errors.append(f"L{e}")
 
-    def check_column(self, board : list[str]):
-        pass
+        # Verifica erros nas colunas
+        for i in range(9):
+            c = self.check(board.col_major[i])
+            for e in c:
+                errors.append(f"C{e}")
 
-    def check_region(self, board : list[str]):
-        pass
+        # Verifica erros nas regiões
+        for i in range(9):
+            c = self.check(board.region_major[i])
+            for e in c:
+                errors.append(f"R{e}")
+        
+        return errors
+           
+    def check(self, board : str):
+        errors = list()
+        # Verificar os caracteres repetidos e guarda a posição do erro
+        for i in range(9):
+            for j in range(i+1, 9):
+                if board[i] == board[j]:
+                    errors.append(j+1)
+        return errors
